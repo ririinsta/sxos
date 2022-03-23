e04eb4bf7dfd949dd778942e9ee7e12e4ea10700
Richard Lung <lungrbackup@protonmail.com>
2022-03-23 11:45:31 -0400

    idk

--- sxos/argsint.cs
+++ sxos/argsint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,15 @@ namespace sxos
     public class argsint
     {
         public void inter(string[] args)
+        {
+            switch (args[0])
+            {
+                case "-c":
+                    string[] lines = File.ReadAllLines(args[1]);
+                    break;
+            }
+        }
+        public void help()
         {
 
         }
---
+++ sxos/interp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sxos
+{
+    public class interp
+    {
+        public void send(string line)
+        {
+            string[] vs = line.Split('.');
+            if (vs[0] == "sys")
+            {
+                sys(line);
+            }
+            else if (vs[0] == "calc")
+            {
+                calc(line);
+            }
+        }
+        public void sys(string line)
+        {
+            string[] vs = line.Split('.');
+            switch (vs[1].Split('(')[0])
+            {
+                case "printline":
+                    Console.WriteLine("tets");
+                    break;
+                default:
+                    Console.WriteLine(line);
+                    break;
+            }
+        }
+        public void calc(string line)
+        {
+
+        }
+    }
+}
